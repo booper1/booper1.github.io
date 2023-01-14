884ab45afbf3124d8461b56021cf17cdb8faa0cc
Cooper Baumgarth <cooperbaumgarth@gmail.com>
2023-01-14 00:51:48 -0500

    Significant progress on the Projects page, 3 cards with near complete styling

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
+    loremIpsum: string = `This is just a sample of text, nothing much to see here. I hope you like it!`;
+
+    projects: { title: string; summary: string; moreInfo: boolean }[] = [
+        {
+            title: "My Personal Portfolio",
+            summary: `This website <i>is</i> my personal portfolio!
+                    I've created this site to showcase a little bit of what I can do, and to share some things about myself.
+                    I hope you like it!`,
+            moreInfo: true
+        },
+        {
+            title: "Running Key Cipher Tool",
+            summary: this.loremIpsum,
+            moreInfo: false
+        },
+        {
+            title: "Medpace Training Tree",
+            summary: this.loremIpsum,
+            moreInfo: true
+        }
+    ];
 }
